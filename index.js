0857904fbea57cf02460c380981033e4044aa869
José Quintana <76418657+SlowpyWalnout@users.noreply.github.com>
2024-04-21 14:52:59 -0600

    actualizacion del uso del path?

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`Hello World from port ${port}! `)
-  console.log(process.env)
+  console.log(process.env.PORT)
+});
+
+app.get('/myname/:name/:edad', (req, res) => { // :name es un parámetro de la URL
+  const name = req.params.name; // req.params es un objeto que contiene los parámetros de la URL
+  const edad = req.params.edad; 
+  res.send(`Hello, my name is ${name} and im ${edad} years old!`);
 });
 
 app.listen(port, () => {
